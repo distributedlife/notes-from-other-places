d25c546f55636bbfa7fd368d0d40e0d75d760d65
Ryan Boucher <ryan.boucher@distributedlife.com>
2013-10-30 22:31:39 +1100

    change bucket

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo `git rev-parse HEAD` > source/version.html
 middleman build
 cd build
-s3cmd sync --acl-public --guess-mime-type --recursive . s3://notes-from-other-places.com/
+s3cmd sync --acl-public --guess-mime-type --recursive . s3://notes-from.distributedlife.com/
 cd ..
 rm -rf build
