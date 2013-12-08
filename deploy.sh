404d37ca08fc5f08bdbe3f6a4ec98afaf0d51112
Ryan Boucher <ryan.boucher@distributedlife.com>
2013-12-08 11:58:18 +1100

    make mime type application/xml for rss

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ bundle install
 bundle exec middleman build
 cd build
 s3cmd sync --acl-public --guess-mime-type --recursive . s3://distributedlife.com/
+s3cmd put travel/rss.xml s3://distributedlife.com/travel/rss.xml --mime-type "application/xml" --acl-public
 s3cmd sync --acl-public --guess-mime-type ~/cron.log s3://distributedlife.com/
 cd ..
 rm -rf build
