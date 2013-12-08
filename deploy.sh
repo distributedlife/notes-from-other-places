17bbebe6e8a1d982fd587d0a80d654854929617a
Ryan Boucher <ryan.boucher@distributedlife.com>
2013-12-08 11:55:30 +1100

    put the logs into s3

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ bundle install
 bundle exec middleman build
 cd build
 s3cmd sync --acl-public --guess-mime-type --recursive . s3://distributedlife.com/
+s3cmd sync --acl-public --guess-mime-type ~/cron.log s3://distributedlife.com/
 cd ..
 rm -rf build
