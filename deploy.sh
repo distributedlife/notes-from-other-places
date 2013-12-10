60480134bf63d9ec55a9040ef9b7e382b695bccf
Ryan Boucher <ryan.boucher@distributedlife.com>
2013-12-10 10:45:48 +1030

    build with verbose logging

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ date
 echo `git rev-parse HEAD` > source/version.html
 gem install bundler --no-rdoc --no-ri
 bundle install
-bundle exec middleman build
+bundle exec middleman build --verbose
 cd build
 s3cmd sync --acl-public --guess-mime-type --recursive . s3://distributedlife.com/
 s3cmd put travel/rss.xml s3://distributedlife.com/travel/rss.xml --mime-type "application/xml" --acl-public
