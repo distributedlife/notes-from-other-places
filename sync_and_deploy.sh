59020074d92a35ae06d8c5ee698177697d2b9f4f
Ryan Boucher <ryan.boucher@distributedlife.com>
2015-05-19 14:01:42 +1000

    trial instapush for deployments

--- sync_and_deploy.sh
+++ sync_and_deploy.sh
@@ -2,4 +2,9 @@ rm source/version.html
 git checkout .
 git pull origin master
 ./deploy.sh
-date
+curl -X POST \
+      -H "x-instapush-appid: $INSTAPUSH_APPID" \
+      -H "x-instapush-appsecret: $INSTAPUSH_APPSECRET" \
+      -H "Content-Type: application/json" \
+      -d '{"event":"deploy","trackers":{"product":"Notes From Other Places"}}' \
+      https://api.instapush.im/post
